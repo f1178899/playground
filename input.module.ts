1aefd9af2f3dad7833bafea99d0737e0531d4a65
Delong <f1178899@gmail.com>
2017-02-10 15:34:21 +1100

    test

---
+++ input.module.ts
@@ -0,0 +1,14 @@
+// modules
+import { NgModule }             from '@angular/core';
+
+// components
+import { TestInputComponent }   from './input.component';
+
+@NgModule({
+    bootstrap: [],
+    declarations: [TestInputComponent],
+    imports: [NgModule],
+    providers: [],
+    entryComponents: [TestInputComponent]
+})
+export class TestInputModule { }
