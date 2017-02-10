9d59d2af8ca898830c3500452baf306059f41f5c
Delong <f1178899@gmail.com>
2017-02-10 14:56:09 +1100

    test

---
+++ input.component.ts
@@ -0,0 +1,14 @@
+import {
+    Component,
+    ViewEncapsulation
+} from '@angular/core';
+
+@Component({
+    selector: 'test-input',
+    template: './input.component.html',
+    styles: ['./input.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+
+
+export class TestInputComponent { }
